cfdc2a666dbcfca805502482febc4f50a35bad07
Subilan <suyuanliang1995@gmail.com>
2021-10-16 21:53:06 +0800

    remove dependency

--- src/main/java/top/seatide/remote/Main.java
+++ src/main/java/top/seatide/remote/Main.java
@@ -1,15 +1,6 @@
 package top.seatide.remote;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
-
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.ess3.api.IEssentials;
 import top.seatide.remote.Utils.Files;
 import top.seatide.remote.Utils.LogUtil;
 import top.seatide.remote.Web.API;
@@ -17,8 +8,6 @@ import top.seatide.remote.Web.Request;
 
 public final class Main extends JavaPlugin {
     public API api;
-    public ProtocolManager man;
-    public IEssentials ess;
 
     @Override
     public void onEnable() {
@@ -32,27 +21,6 @@ public final class Main extends JavaPlugin {
             e.printStackTrace();
             LogUtil.error("无法启动 HTTP 服务器。");
         }
-        man = ProtocolLibrary.getProtocolManager();
-        man.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.CHAT) {
-            @Override
-            public void onPacketSending(PacketEvent e) {
-                System.out.println("Listening packet event.");
-                var pack = e.getPacket();
-                var msg = pack.getStrings().read(0);
-                System.out.println(msg);
-            }
-        });
-        man.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Client.CHAT) {
-            @Override
-            public void onPacketReceiving(PacketEvent e) {
-                System.out.println("Listening packet event.");
-                var pack = e.getPacket();
-                var msg = pack.getStrings().read(0);
-                System.out.println(msg);
-            }
-        });
-        // ess = (IEssentials) Bukkit.getPluginManager().getPlugin("Essentials");
-        // System.out.println(ess.getTimer().getAverageTPS());
     }
 
     @Override
